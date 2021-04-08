26f9ef468391dae85abb9ac7ce6661d77de295e6
Pavel Kalganov <piano@Pianos-MacBook-Pro.local>
2021-04-08 14:01:23 +0400

    Add desktop styles for catalog page

--- app/js/beforeAfter.js
+++ app/js/beforeAfter.js
@@ -3,7 +3,6 @@ let imgAfter = document.querySelector(".example__img--after");
 let range = document.querySelector(".example__slider-input");
 let beforeButton = document.querySelector(".example__input-text--before");
 let afterButton = document.querySelector(".example__input-text--after");
-let mapElement = document.querySelector(".map");
 
 range.addEventListener("input", () => {
   imgBefore.style.width = 100 - range.value + "%";
@@ -23,22 +22,3 @@ afterButton.addEventListener("click", () => {
 
   range.value = 100;
 });
-
-function initMap() {
-  let image = "../images/map-pin.png";
-  let resolutionFix = { lat: 59.938786753250206, lng: 30.322068231597587 };
-
-  var coordinates = { lat: 59.93873690835845, lng: 30.323096456467916 },
-    map = new google.maps.Map(document.querySelector(".map__google-frame"), {
-      center: resolutionFix,
-      zoom: 18,
-    }),
-    marker = new google.maps.Marker({
-      position: coordinates,
-      icon: image,
-      map: map,
-      animation: google.maps.Animation.BOUNCE,
-    });
-}
-
-initMap();
